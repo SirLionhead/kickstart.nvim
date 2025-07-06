6c9844407ffae9833c5ac8d01c3681a46195f3a0
Jordan Berg <sir@Jumba.local>
2025-07-06 00:34:18 -0500

    Moved plugins to custom/plugins and added vim-fugitive

---
+++ lua/custom/keymaps.lua
@@ -0,0 +1,11 @@
+-- [[ Basic Keymaps ]]
+vim.keymap.set('n', '<leader>w', '<cmd>write<CR>', { desc = '[W]rite' })
+vim.keymap.set('n', '<leader>q', '<cmd>quit<CR>', { desc = '[Q]uit' })
+vim.keymap.set('n', '<leader>o', '<C-w>o', { desc = '[O]nly Window' })
+vim.keymap.set('n', '<leader>f', '<cmd>tab Git<CR>', { desc = '[F]ugitive' })
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
